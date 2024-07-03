d5e4e961daf87120c3cf8eb6cc34e4ac14bc6956
martopad <martin_luther_yung@outlook.com>
2024-07-03 17:22:53 +0200

    Config changes 1) moved config.ini to be machine specific (use --config-file flag) 2) Added inline comment pruning when parsing config file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 import configparser
 import os
 import pathlib
+import re
 import subprocess
 import sys
 
@@ -22,6 +23,11 @@ variables_to_inject = {
     "${INJECTED_BASE_DIR}": base_dir
     } 
 
+def strip_comments(code):
+    # so that inline comments are in the ini file is possible
+    code = str(code)
+    return re.sub(r'(?m) *#.*\n?', '', code)
+
 class InterpolationWithEnvVarsAndCustomDictionary(configparser.ExtendedInterpolation):
     def before_read(self, parser, section, option, value):
         value = super().before_read(parser, section, option, value)
@@ -29,7 +35,7 @@ class InterpolationWithEnvVarsAndCustomDictionary(configparser.ExtendedInterpola
         var_injected_value = env_expanded_value
         for key, value in variables_to_inject.items():
             var_injected_value = var_injected_value.replace(key, value)
-        return var_injected_value
+        return strip_comments(var_injected_value)
 
 user_conf = configparser.ConfigParser(interpolation = InterpolationWithEnvVarsAndCustomDictionary())
 user_conf.optionxform = str # preserve field-name cases
@@ -53,11 +59,6 @@ def run_bash_script_chrooted(filename: str, path_to: str) -> None:
     run_cmd([f"{base_dir}/exec_as_chroot.sh", chrooted_dest])
     run_cmd(["rm", "-f", f"{user_conf['ARGPY']['ARGPY_MNT_ROOT']}/{chrooted_dest}"])
 
-#subprocess.run(args=["which", "bzip2"])
-#subprocess.run(args=["which", "bzip2"], check=True, shell=True)
-
-#print(user_conf['ARGPY']['ARGPY_DRIVE_TARGET'])
-#run_cmd([f"{base_dir}/test.sh"])
 def extract_number_from_file(file: pathlib.Path) -> int:
     filename: str = file.stem
     splitted = filename.split("_")
@@ -65,8 +66,6 @@ def extract_number_from_file(file: pathlib.Path) -> int:
  
 pre_chroot_scripts = pathlib.Path(f"{user_conf['ARGPY']['ARGPY_DIRS_SCRIPTS']}/pre_chroot")
 to_chroot_scripts = pathlib.Path(f"{user_conf['ARGPY']['ARGPY_DIRS_SCRIPTS']}/to_chroot")
-#print(to_chroot_scripts.iterdir())
-
 
 if int(user_conf['CONTROL']['DO_1']):
     run_cmd([f"{pre_chroot_scripts}/1_provision_fs.sh"])
@@ -79,35 +78,20 @@ if int(user_conf['CONTROL']['DO_2']):
     run_cmd([f"{pre_chroot_scripts}/2_install_system.sh"])
 
 if int(user_conf['CONTROL']['DO_3']):
-    #run_cmd(["mkdir", "--parents", user_conf['ARGPY']['ARGPY_MNT_BOOT']])
-    #run_cmd(["mount", user_conf['ARGPY']['ARGPY_PART_BOOT'], user_conf['ARGPY']['ARGPY_MNT_BOOT']])
+    run_cmd(["mkdir", "--parents", user_conf['ARGPY']['ARGPY_MNT_BOOT']])
+    run_cmd(["mount", user_conf['ARGPY']['ARGPY_PART_BOOT'], user_conf['ARGPY']['ARGPY_MNT_BOOT']])
     #arch-chroot behavior, wrapping it in python doesnt inherit shell env
-    #run_cmd([f"{base_dir}/3_setup_system.sh"])
-    #run_cmd(["rsync", "-a", f"{user_conf['ARGPY']['CFG_DIRS_SCRIPTS']}/root/", user_conf['ARGPY']['ARGPY_MNT_ROOT']])
-    #run_cmd(["chown", "-R", "root:root", user_conf['ARGPY']['ARGPY_MNT_ROOT']])
+    run_cmd(["rsync", "-a", f"{user_conf['ARGPY']['ARGPY_DIRS_SCRIPTS']}/root/", user_conf['ARGPY']['ARGPY_MNT_ROOT']])
+    run_cmd(["chown", "-R", "root:root", user_conf['ARGPY']['ARGPY_MNT_ROOT']])
     listified = list(to_chroot_scripts.iterdir())
     listified.sort(key=extract_number_from_file)
     for script in listified:
-        #run_bash_script_chrooted(script.name, script.parent)
-        print(script.name)
-        print(script.parent)
+        run_bash_script_chrooted(script.name, script.parent)
+        #print(script.name)
+        #print(script.parent)
     #run_bash_script_chrooted("test.sh", f"{base_dir}/root/after_chroot")
 #TODO missing fstab configuration for additional storage
 if int(user_conf['CONTROL']['DO_4']):
     run_cmd(["swapoff", user_conf['ARGPY']['ARGPY_PART_SWP']]) #TODO: stateful action across runs
     run_cmd(["umount", "-R", user_conf['ARGPY']['ARGPY_MNT_ROOT']])
-
-
-#${BASE_DIR}/1_provision_fs.sh
-#swapon "${PART_SWP}"
-
-#mkdir --parents "${MNT_ROOT}"
-#mount "${PART_ROOT}" "${MNT_ROOT}"
-#${BASE_DIR}/2_install_system.sh "${MNT_ROOT}" "amd64" "openrc"
-
-#mkdir --parents "${MNT_BOOT}"
-#mount "${PART_BOOT}" "${MNT_BOOT}"
-#${BASE_DIR}/3_setup_system.sh "${MNT_ROOT}"
-
-#umount -R "${MNT_ROOT}"
 
